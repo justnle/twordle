256c4ebcd67a669869a181021046ecafde2ad8d7
Justin <me@justinle.page>
2022-03-28 15:45:46 -0700

    cleaned up unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import WordRow from './WordRow';
 
-function App() {
-    const [count, setCount] = useState(0);
-
+export default function App() {
     return (
         <div className="mx-auto w-96">
-            <h1 className="text-4xl text-center">Twordle</h1>
+            <header className="border-b border-gray-500 pb-2 my-2">
+                <h1 className="text-4xl text-center">Twordle</h1>
+            </header>
+
+            <main>
+                <WordRow letters="hel" />
+                <WordRow letters="hell" />
+                <WordRow letters="hello" />
+            </main>
         </div>
     );
 }
-
-export default App;
