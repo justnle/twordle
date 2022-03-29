848e45809adc88b03710a73cf207bb6cc394c243
Justin <info@justinle.me>
2022-03-29 15:03:55 -0700

    modal for newGame when guesses === guess_length

--- src/App.tsx
+++ src/App.tsx
@@ -29,8 +29,10 @@ export default function App() {
 
     rows = rows.concat(Array(remainingGuesses).fill(``));
 
+    const gameStatus = state.guesses.length === GUESS_LENGTH;
+
     return (
-        <div className="mx-auto w-96">
+        <div className="mx-auto w-96 relative">
             <header className="border-b border-gray-500 pb-2 my-2">
                 <h1 className="text-4xl text-center">Twordle</h1>
 
@@ -40,6 +42,7 @@ export default function App() {
                         className="border-2 border-gray-500 p-2 w-half"
                         value={guess}
                         onChange={handleChange}
+                        disabled={gameStatus}
                     />
                 </div>
             </header>
@@ -49,6 +52,24 @@ export default function App() {
                     <WordRow key={`${index}-${word}`} letters={word} />
                 ))}
             </main>
+
+            {gameStatus && (
+                <div
+                    role="modal"
+                    className="absolute bg-white left-0 right-0 top-1/4 p-6 w-3/4 mx-auto rounded border border-gray-500 text-center"
+                >
+                    Game Over!
+                    <button
+                        className="block border rounded border-green-500 bg-green-500 p-2 mt-4 mx-auto shadow"
+                        onClick={() => {
+                            state.newGame();
+                            setGuess(``);
+                        }}
+                    >
+                        New Game
+                    </button>
+                </div>
+            )}
         </div>
     );
 }
