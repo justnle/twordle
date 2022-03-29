d1d7b2776012bb6da4779a6235dbac56bcb005aa
Justin <me@justinle.page>
2022-03-28 22:14:58 -0700

    tests for the word guessing algo

--- src/word-utils.test.ts
+++ src/word-utils.test.ts
@@ -1,9 +1,76 @@
 import { describe, expect, it } from 'vitest';
-import { getRandomWord } from './word-utils';
+import { computeGuess, getRandomWord, LetterState } from './word-utils';
 
-describe(`word-utils`, () => {
+describe(`getRandomWord`, () => {
     it(`random word`, () => {
         expect(getRandomWord()).toBeTruthy();
         expect(getRandomWord().length).toEqual(5);
+    });
+});
+
+describe(`computeGuess`, () => {
+    it(`works with match and present`, () => {
+        // computeGuess(guess, answer)
+        expect(computeGuess(`kabar`, `kirby`)).toEqual([
+            LetterState.Match,
+            LetterState.Miss,
+            LetterState.Present,
+            LetterState.Miss,
+            LetterState.Present
+        ]);
+    });
+
+    it(`works with all matches`, () => {
+        expect(computeGuess(`mario`, `mario`)).toEqual([
+            LetterState.Match,
+            LetterState.Match,
+            LetterState.Match,
+            LetterState.Match,
+            LetterState.Match
+        ]);
+    });
+
+    it(`works with all misses`, () => {
+        expect(computeGuess(`traps`, `luigi`)).toEqual([
+            LetterState.Miss,
+            LetterState.Miss,
+            LetterState.Miss,
+            LetterState.Miss,
+            LetterState.Miss
+        ]);
+    });
+
+    it(`works with double letters, matching only one, not both`, () => {
+        expect(computeGuess(`stock`, `smash`)).toEqual([
+            LetterState.Match,
+            LetterState.Miss,
+            LetterState.Miss,
+            LetterState.Miss,
+            LetterState.Miss
+        ]);
+    });
+
+    test(`returns an empty array when given an incomplete guess`, () => {
+        expect(computeGuess(`to`, `zelda`)).toEqual([]);
+    });
+
+    test(`when two letters are present, but answer has only one of the letters`, () => {
+        expect(computeGuess(`koopa`, `scorn`)).toEqual([
+            LetterState.Miss,
+            LetterState.Miss,
+            LetterState.Match,
+            LetterState.Miss,
+            LetterState.Miss
+        ]);
+    });
+
+    test(`when one letter matches, but guess has more of the same letter`, () => {
+        expect(computeGuess(`dough`, `diddy`)).toEqual([
+            LetterState.Match,
+            LetterState.Miss,
+            LetterState.Miss,
+            LetterState.Miss,
+            LetterState.Miss
+        ]);
     });
 });
