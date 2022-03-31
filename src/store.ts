6cfe1104565407c243deb9ede74260e3e23421ef
Justin <info@justinle.me>
2022-03-31 01:33:26 -0700

    added keyboard state

--- src/store.ts
+++ src/store.ts
@@ -3,7 +3,6 @@ import { persist } from 'zustand/middleware';
 import { computeGuess, getRandomWord, LetterState } from './word-utils';
 
 export const GUESS_LENGTH = 6;
-
 interface GuessRow {
     guess: string;
     result?: LetterState[];
@@ -13,6 +12,7 @@ interface StoreState {
     answer: string;
     rows: GuessRow[];
     gameState: `active` | `success` | `fail`;
+    keyboardLetterState: { [letter: string]: LetterState };
     addGuess: (guess: string) => void;
     newGame: (initialGuess?: string[]) => void;
 }
@@ -23,6 +23,7 @@ export const useStore = create<StoreState>(
             const addGuess = (guess: string) => {
                 const result = computeGuess(guess, get().answer);
                 const succeed = result.every((i) => i === LetterState.Match);
+
                 const rows = [
                     ...get().rows,
                     {
@@ -31,8 +32,28 @@ export const useStore = create<StoreState>(
                     }
                 ];
 
+                const keyboardLetterState = get().keyboardLetterState;
+                result.forEach((r, index) => {
+                    const resultGuessLetter = guess[index];
+                    const currentLetterState =
+                        keyboardLetterState[resultGuessLetter];
+
+                    switch (currentLetterState) {
+                        case LetterState.Match:
+                            break;
+                        case LetterState.Present:
+                            if (r === LetterState.Miss) {
+                                break;
+                            }
+                        default:
+                            keyboardLetterState[resultGuessLetter] = r;
+                            break;
+                    }
+                });
+
                 set(() => ({
                     rows,
+                    keyboardLetterState,
                     gameState: succeed
                         ? `success`
                         : rows.length === GUESS_LENGTH
@@ -45,12 +66,14 @@ export const useStore = create<StoreState>(
                 answer: getRandomWord(),
                 rows: [],
                 gameState: `active`,
+                keyboardLetterState: {},
                 addGuess,
                 newGame: (initialRows = []) => {
                     set({
                         answer: getRandomWord(),
                         rows: [],
-                        gameState: `active`
+                        gameState: `active`,
+                        keyboardLetterState: {}
                     });
 
                     initialRows.forEach(addGuess);
