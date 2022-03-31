8a1254b3139120b79cf517adb6f3199bd46b6b08
Justin <info@justinle.me>
2022-03-31 01:33:16 -0700

    Implemented Key letter states

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -1,10 +1,16 @@
+import { useStore } from './store';
+import { LetterState } from './word-utils';
+
 export default function Keyboard({
     onClick: onClickProp
 }: {
     onClick: (letter: string) => void;
 }) {
+    const keyboardLetterState = useStore((s) => s.keyboardLetterState);
+
     const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
         const letter = e.currentTarget.textContent;
+
         onClickProp(letter!);
     };
 
@@ -18,13 +24,17 @@ export default function Keyboard({
                     >
                         {keyboardRow.map((key, index) => {
                             let styles = `rounded font-bold uppercase py-2 flex-1`;
-
-                            if (key !== ``) {
-                                styles += ` bg-gray-400`;
-                            }
+                            const letterState =
+                                keyboardStateStyles[keyboardLetterState[key]];
 
                             if (key === ``) {
                                 styles += ` pointer-events-none`;
+                            }
+
+                            if (letterState) {
+                                styles += ` ${letterState}`;
+                            } else if (key !== ``) {
+                                styles += ` bg-gray-400`;
                             }
 
                             return (
@@ -49,3 +59,9 @@ const keyboardKeys = [
     [``, `a`, `s`, `d`, `f`, `g`, `h`, `j`, `k`, `l`, ``],
     [`Enter`, `z`, `x`, `c`, `v`, `b`, `n`, `m`, `Backspace`]
 ];
+
+const keyboardStateStyles = {
+    [LetterState.Miss]: 'bg-gray-500',
+    [LetterState.Present]: 'bg-yellow-500',
+    [LetterState.Match]: 'bg-green-500'
+};
